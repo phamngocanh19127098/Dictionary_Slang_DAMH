c83b92f2f529b7b64e46b7bb8edf30190f0b91f3
phamngocanh19127098 <npham4533@gmail.com>
2021-12-17 09:35:38 +0700

    add gui

--- src/Dictionary_Slang.java
+++ src/Dictionary_Slang.java
@@ -410,7 +410,71 @@ public class Dictionary_Slang {
         Object[] convertedToArray = data_word.keySet().toArray();
         return (String)convertedToArray[new Random().nextInt(convertedToArray.length)];
     }
+    public void GUI(){
+        while (true){
+            System.out.println("\n<----------------------------------------------------------------------->");
+            System.out.println("1.  Slang Search");
+            System.out.println("2.  Definition Search");
+            System.out.println("3.  Show Slang Search History");
+            System.out.println("4.  Add Slang");
+            System.out.println("5.  Edit Slang");
+            System.out.println("6.  Delete Slang");
+            System.out.println("7.  Reset dictionary");
+            System.out.println("8.  On this day slang word");
+            System.out.println("9.  Slang quiz");
+            System.out.println("10.  Definition quiz");
+            System.out.println("0.  Exit");
+            System.out.print("Enter a number to select task: ");
+            String choice = scanner.nextLine();
+            switch(choice){
+                case "1":
+                    System.out.print("Enter slang word : ");
+                    String word = scanner.nextLine();
+                    searchWordSlang(word);
+                    showWord(word);
+                    break;
+                case "2":
+                    System.out.print("Enter definition: ");
+                    String def = scanner.nextLine();
+                    List<String>result = searchByDefinition(def);
+                    int lengthResult = result.size();
+                    if(lengthResult!=0){
+                        System.out.println("Find "+lengthResult+" slang word");
+                        for(String i:result){
+                            showWord(i);
+                        }
+                    }
+                    else {
+                        System.out.println("There no result to display");
+                    }
+                    break;
+                case "3":
+                    for(String i : history){
+                        System.out.println(i);
+                    }
+                    break;
+                case "4":
+                    addSlangWord();
+                    break;
+                case "5":
+                    editSlangWord();
+                    break;
+                case "6":
+                    deleteSlangWord();
+                    break;
+                case "7":
+                    resetDictionary();
+                    break;
+                case "8":
+                    showWord(thisDaySlangWord);
+                    break;
+                case "0":
+                    return;
+            }
 
+        }
+
+    }
     public static void main(String[] args) {
 	// write your code here
         Dictionary_Slang dictionary_slang = new Dictionary_Slang();
